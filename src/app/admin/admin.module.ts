2b4f38398b8fa2a864dc5a3247b4deb0eb791a9f
Gustavo Gutiérrez <gustavogutierrezmercado@gmail.com>
2018-03-26 00:08:43 -0500

    Progres integration with Angular Material

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminRoutingModule } from './admin-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
-import { AppStoreModule } from '../store/app.store'
+import { AppStoreModule } from '../store/app.store';
 
 @NgModule({
   imports: [
     CommonModule,
     AdminRoutingModule,
-    AppStoreModule
+    AppStoreModule,
   ],
   declarations: [
     DashboardComponent
   ],
   exports: [
-    DashboardComponent
-  ]
+    DashboardComponent,
+  ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
 export class AdminModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { CoreModule, LayoutComponent } from './core';
 
 @NgModule({
   imports: [
     BrowserModule,
-    CoreModule
+    BrowserAnimationsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [ LayoutComponent ]
