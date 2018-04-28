e4392d05d0f426298bce95a903ef0cd9c3009583
Gustavo Gutierrez <gustavogutierrezmercado@gmail.com>
2018-04-28 15:32:30 -0500

    add utils shared files

---
+++ src/app/shared/utils/data.utils.ts
@@ -0,0 +1,7 @@
+import { SimpleChange } from '@angular/core';
+
+export const isNewChange = (prop: SimpleChange) => {
+  return prop.currentValue !== prop.previousValue;
+};
+
+export const toPayload = (action: any) => action.payload;
